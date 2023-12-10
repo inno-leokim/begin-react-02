0af4342823e28f62ef0b83af5986fd058c8da4d5
inno-leokim <inno.yunkim@gmail.com>
2023-12-10 11:06:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
+
+    const [inputs, setInputs] = useState({
+      username: '',
+      email: ''
+    });
+
+    const { username, email } = inputs;
+
+    const onChange = e => {
+      e.preventDefault();
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      });
+    };
+
+    const [users,setUsers] = useState([
+        {
+          id: 1,
+          username: 'velopert',
+          email: 'public.velopert@gmail.com',
+          active: true
+        },
+        {
+          id: 2,
+          username: 'tester',
+          email: 'tester@example.com',
+          active: false
+        },
+        {
+          id: 3,
+          username: 'liz',
+          email: 'liz@example.com',
+          active: false
+        }
+  ]);
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+
+      const user = {
+        id: nextId.current,
+        username,
+        email
+      }
+
+      // setUsers([...users, user]);
+
+      setUsers(users.concat(user));
+
+      setInputs({
+        username: '',
+        email: ''
+      })
+      nextId.current += 1;
+  };
+
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  }
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user => 
+        user.id === id ? {...user, active: !user.active} : user  
+      )
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreateUser 
+        username={username} 
+        email={email} 
+        onCreate={onCreate} 
+        onChange={onChange}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </div>
   );
 }
